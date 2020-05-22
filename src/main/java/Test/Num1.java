1f54876ddd25bd266fca540ef789b560f74376d8
Krish Srivatsav <krish@cornhub.gov>
2020-05-22 13:59:27 +0530

    first commit

---
+++ src/main/java/Test/Num1.java
@@ -0,0 +1,19 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Num1 {
+	
+	@Test
+	public void add()
+	{
+		System.out.println("hello");
+	}
+	
+	@Test
+	public void sub()
+	{
+		System.out.println("hii");
+	}
+
+}
---
+++ src/main/java/Test/Num2.java
@@ -0,0 +1,11 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Num2 {
+
+	@Test
+	public void addtwo() {
+		System.out.println("hello two");
+	}
+}
